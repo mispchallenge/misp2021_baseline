7a75edbc04f68e327d114e74927fba3307b42027
Zhaoxu Nian <60314608+elissopp@users.noreply.github.com>
2021-11-24 21:39:42 +0800

    Update run.sh

--- task1_wws/data_prepare/run.sh
+++ task1_wws/data_prepare/run.sh
@@ -19,8 +19,10 @@ ${python_path}python add_reverb/add_reverb.py --scp_path add_reverb/positive.scp
 # add_noise
 echo "start adding middle noise "
 ${python_path}python add_noise/process_noise.py --data_root $data_root
+chmod 754 add_noise/AddNoise_MultiOutput
 mkdir -p add_noise/scp
 mkdir -p add_noise/Config
+mkdir -p add_noise/log
 ${python_path}python add_noise/process_data.py --data_root $data_root --scp_path add_noise/scp/positive.scp
 perl add_noise/Add_noise_MultipleOutput.pl
 rm add_noise/*/*[0-9]
